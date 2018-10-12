2d7d6831da08d234f505e2436519db4a2554fa20
moongar <moosa.moghimi@gmail.com>
2018-10-12 16:32:27 -0600

    now MathOperation class and 4 basic operation really added and commited

--- src/com/moses/Main.java
+++ src/com/moses/Main.java
@@ -3,6 +3,11 @@ package com.moses;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a=2;
+        int b=3;
+        System.out.println(a+" + "+b+" = "+MathOpertions.add(a,b));
+        System.out.println(a+" - "+b+" = "+MathOpertions.subtract(a,b));
+        System.out.println(a+" X "+b+" = "+MathOpertions.multiply(a,b));
+        System.out.printf(a+" / "+b+" = "+MathOpertions.divide(a,b));
     }
 }
